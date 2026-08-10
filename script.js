c85e93910e82224f1fb30348875725855c09f1c6
Mahmuda Akter <mahmudapakter@gmail.com>
2026-08-10 12:25:15 +0600

    version 3 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const secretNumber = Math.floor(Math.random() * 100 ) + 1 ;
+let secretNumber = Math.floor(Math.random() * 100 ) + 1 ;
+console.log(secretNumber); // For testing purposes, you can remove this line in production
 
 const guessInput = document.getElementById("guessInput");
 const guessButton = document.getElementById("guessButton");
@@ -9,19 +10,20 @@ let attempts = 0;
 guessButton.addEventListener("click", () => { 
     const guess = Number(guessInput.value);
 
-    if (guess < secretNumber) {
-        message.textContent = "Too low! Try again";
-
-    }
-
-    else if (guess > secretNumber) {
-        message.textContent = "Too high! Try again";
+    if(guess < 1 || guess > 100) {
+        message.textContent = "Please enter a number between 1 and 100.";
     }
     else {
-        message.textContent = "Congratulations! You guessed the number!";
-        
-    }
-
+        if (guess < secretNumber) {
+            message.textContent = "Too low! Try again";
+        }
+        else if (guess > secretNumber) {
+            message.textContent = "Too high! Try again";
+        }
+        else {
+          message.textContent = "Congratulations! You guessed the number!";
+        }
     attempts++;
+    }
     attemptsElement.textContent = `Attempts: ${attempts}`;
 });
